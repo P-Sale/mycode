042db9994fd9271929a9a1286776c44bffa24d32
P-Sale <paul.a.sale.01@gmail.com>
2023-02-21 18:42:17 +0000

    join and split, and previous files

---
+++ lab_input/iptaker02.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def main():
+    user_input = input("please enter an IPv4 address: ")
+
+    print("You told me the IPv4 address is : ", user_input)
+
+    vendor = input("please input the vendor\'s name: ")
+
+    print("You told me the vendor is ", vendor)
+
+main()
+
---
+++ py3_shebang.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+print("Hello, again.")
+print("oh, hello there.")
+
