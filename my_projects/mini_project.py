e440e253f653dcffa3b907336b72282338cc05ea
P-Sale <paul.a.sale.01@gmail.com>
2023-02-24 22:04:44 +0000

    added input validation to mini project

--- my_projects/mini_project.py
+++ my_projects/mini_project.py
@@ -23,9 +23,15 @@ def main():
         while turn>0:
             if win == 0:
                 print(f"Congratulations! You guessed the secret word {word}!")
-                replay = input("Would you like to play again? y/n: ")
-                if replay == 'n':
-                    play = False
+                while True:
+                    replay = input("Would you like to play again? y/n: ")
+                    if replay.lower() == 'n':
+                        play = False
+                        break
+                    elif replay.lower() == 'y':
+                        break
+                    else:
+                        print("Please input either y or n.")
                 clear_console()
                 break
             print("Guesses remaining: ", turn)
@@ -40,26 +46,41 @@ def main():
             for letter in guesses:
                 print(letter, end='')
 
-            guess=input("\nGuess a letter: ")
-
-            if testLetter(guess, word) == True:
-                input(f"Correct! {guess} is in the secret word.")
-                guesses.append(guess)
-                for letter in word:
-                    if letter == guess:
-                        win = win-1
-            else:
-                input(f"Sorry, {guess} is not in the secret word.")
-                guesses.append(guess)
+            while True:
+                guess=input("\nGuess a letter: ")
+            
+                if guess == int:
+                    print("Input a letter, numbers are not allowed.")
+                elif guess == '':
+                    print("Input a letter.")
+                elif guess.lower() in guesses:
+                    print(f"You already guessed {guess}, pick a different letter.")
+                elif testLetter(guess.lower(), word) == True:
+                    input(f"Correct! {guess} is in the secret word.")
+                    guesses.append(guess.lower())
+                    for letter in word:
+                        if letter == guess.lower():
+                            win = win-1
+                    break
+                else:
+                    input(f"Sorry, {guess} is not in the secret word.")
+                    guesses.append(guess.lower())
+                    break
             
             turn=turn-1
 
             clear_console()
         if win>0:
             print(f"Sorry, you did not guess the secret word {word}.")
-            replay = input("Would you like to play again? y/n: ")
-            if replay == 'n':
-                play = False
+            while True:
+                replay = input("Would you like to play again? y/n: ")
+                if replay.lower() == 'n':
+                    play = False
+                    break
+                elif replay.lower() == 'y':
+                    break
+                else:
+                    print("Please input either y or n.")
             clear_console()
     
     input("Thank you for playing secret word!")
