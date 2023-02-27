5cc01729f2333d5ceb56f92c7ecc7d677e56184c
P-Sale <paul.a.sale.01@gmail.com>
2023-02-27 23:21:15 +0000

    csv and pandas

---
+++ csvread02.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import csv
+
+def main():
+    with open('superbirthday.csv', mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                                                              
+                print('Column names are {}'.format(", ".join(row)))
+                line_count += 1
+            
+            print('\t{} aka {} was born in {}.'.format(row["name"],row["heroname"],row["birthday month"]))
+            line_count += 1
+
+    print('Processed {} lines.'.format(line_count))
+if __name__ == "__main__":
+    main()
+
